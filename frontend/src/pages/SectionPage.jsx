e179fa44c49aab70f6c308192b5eee6bd3c77156
Injun Jeong <jijun897@naver.com>
2025-05-18 19:09:53 +0900

    Feat(FE): section 페이지 제작 후 더미 데이터를 이요하여 그래프 생성

--- frontend/src/pages/SectionPage.jsx
+++ frontend/src/pages/SectionPage.jsx
@@ -1,10 +1,46 @@
+import styled from "styled-components";
 import TheHeader from "../layout/TheHeader";
+import SectionGraph from "../components/SectionGraph";
+
+const GraphDiv = styled.div`
+  width: 80%;
+  margin: 40px auto;
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+`;
+
+const GraphH2 = styled.h2`
+  display: flex;
+  gap: 15px;
+  font-size: 24px;
+  font-weight: 500;
+`;
+
+const Box = styled.div`
+  width: 20px;
+  height: 30px;
+  background-color: #272757;
+`;
+
+const SelectDateDiv = styled.div`
+  width: 90%;
+  font-size: 14px;
+  text-align: right;
+`;
 
 const SectionPage = () => {
   return (
     <>
       <TheHeader />
-      <p>Section page</p>
+      <GraphDiv>
+        <GraphH2>
+          <Box />
+          섹션별 체류 시간
+        </GraphH2>
+        <SelectDateDiv>일간 / 주간 / 월간</SelectDateDiv>
+        <SectionGraph />
+      </GraphDiv>
     </>
   );
 };
