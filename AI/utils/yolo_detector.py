d303d27ff3b21bdae06d41839b91fb27ad9e20d1
Kim Chang Hyeon <gnachkim0119@gmail.com>
2025-06-23 19:49:57 +0900

    model route changed

--- AI/utils/yolo_detector.py
+++ AI/utils/yolo_detector.py
@@ -1,10 +1,11 @@
+# yolo detector --main
+
 from ultralytics import YOLO
 import torch
 
 class YOLODetector:
     def __init__(self):
-        self.model = YOLO("../models/yolo_model_best.pt")
-        # self.model = YOLO("train10/weights/best.pt")
+        self.model = YOLO("models/best.pt")
 
     def detect(self, frame):
         results = self.model.predict(
@@ -12,7 +13,7 @@ class YOLODetector:
             imgsz=1280, 
             conf=0.4, 
             stream=True, 
-            device='cuda' if torch.cuda.is_available else 'cpu')
+            device='cuda' if torch.cuda.is_available() else 'cpu')
         detections = []
         
         for r in results:
